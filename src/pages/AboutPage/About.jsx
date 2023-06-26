4a106f78a6a6a65863edb246ce57ced45337cd18
Владислав Кива <melnikovv717@gmail.com>
2023-06-27 00:46:07 +0300

    update about page

--- src/pages/AboutPage/About.jsx
+++ src/pages/AboutPage/About.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { HeaderBurger } from "../../components/HeaderBurger/HeaderBurger";
 import { Link } from "react-router-dom";
 import "./about.scss";
@@ -9,11 +11,14 @@ import jsIcon from "../../assets/images/icons/js-icon.png";
 import reactIcon from "../../assets/images/icons/react-icon.png";
 import sassIcon from "../../assets/images/icons/sass-icon.png";
 import gitIcon from "../../assets/images/icons/git-icon.png";
-import figmaIcon from "../../assets/images/icons/figma-icon.png";
 import cvIcom from "../../assets/images/icons/cv.png";
 
 
 const About = () => {
+    const [about, setAbout] = useState(true);
+    const [skills, setSkills] = useState(false);
+    const [education, setEducation] = useState(false);
+
 
     return(
         <section className="about">
@@ -34,25 +39,80 @@ const About = () => {
 
                 <div className="about_main">
 
-                    <div className="about_main_text_container">
-                        <h1 className="about_title">Обо мне</h1>
-                        <div className="about_subtitle">
-                            Я - начинающий и амбициозный фронтенд разработчик. Мои знания и опыт позволяют мне успешно решать сложные задачи и создавать качественные продукты.<br/>
-                            Я использую современные технологии HTML, CSS3, JavaScript и React.<br/><br/>
-                            Моя цель - развиваться и улучшать свои навыки, чтобы стать профессионалом в своей области и создавать продукты, которые будут полезны для пользователей.<br/>
-                            Я готов работать в команде и брать на себя ответственность за свои проекты. Если вы ищете талантливого сотрудника, который готов к новым вызовам и развитию, то я рад присоединиться к вашей команде.
+                    <div className="about_task">
+                        <div className="about_header_task">
+
+                            <div className={about ? "header_nav_task_active" : "header_nav_task"}
+                                onClick={() => {
+                                    setAbout(true);
+                                    setSkills(false);
+                                    setEducation(false);
+                                }}
+                            >
+                                Обо мне
+                            </div>
+
+                            <div className={skills ? "header_nav_task_active" : "header_nav_task"}
+                                onClick={() => {
+                                    setAbout(false);
+                                    setSkills(true);
+                                    setEducation(false);
+                                }}
+                            >
+                                Навыки
+                            </div>
+
+                            <div className={education ? "header_nav_task_active" : "header_nav_task"}
+                                onClick={() => {
+                                    setAbout(false);
+                                    setSkills(false);
+                                    setEducation(true);
+                                }}
+                            >
+                                Образование
+                            </div>
+
                         </div>
 
-                        <div className="about_main_skills">
-                            <h1 className="about_title">Знания, навыки</h1>
-                            <div className="about_skills_container">
-                                <div className="skills_span_container"><img src={htmlIcon}/> <span>HTML5</span></div>
-                                <div className="skills_span_container"><img src={cssIcon}/> <span>CSS3</span></div>
-                                <div className="skills_span_container"><img src={jsIcon}/> <span>JavaScript</span></div>
-                                <div className="skills_span_container"><img src={reactIcon}/> <span>React.js</span></div>
-                                <div className="skills_span_container"><img src={sassIcon}/> <span>Sass. Scss</span></div>
-                                <div className="skills_span_container"><img src={gitIcon}/> <span>Git. GitHub. GitLab</span></div>
-                                <div className="skills_span_container"><img src={figmaIcon}/> <span>Figma</span></div>
+                        <div className="about_main_task">
+                            <div className={about ? "main_task_about_active" : "main_task_about"}>
+                                <div className="about_subtitle">
+                                    Я - начинающий и амбициозный фронтенд разработчик. Мои знания и опыт позволяют мне успешно решать сложные задачи и создавать качественные продукты.<br/>
+                                    Я использую современные технологии HTML, CSS3, JavaScript и React.<br/><br/>
+                                    Моя цель - развиваться и улучшать свои навыки, чтобы стать профессионалом в своей области и создавать продукты, которые будут полезны для пользователей.<br/>
+                                    Я готов работать в команде и брать на себя ответственность за свои проекты. Если вы ищете талантливого сотрудника, который готов к новым вызовам и развитию, то я рад присоединиться к вашей команде.
+                                </div>
+                            </div>
+
+                            <div className={skills ? "main_task_skills_active" : "main_task_skills"}>
+                                <div className="skills_title">Основные навыки:</div>
+                                <div className="about_skills_container">
+                                    <div className="skills_span_container"><img src={htmlIcon}/> <span>HTML5</span></div>
+                                    <div className="skills_span_container"><img src={cssIcon}/> <span>CSS3</span></div>
+                                    <div className="skills_span_container"><img src={jsIcon}/> <span>JavaScript</span></div>
+                                    <div className="skills_span_container"><img src={reactIcon}/> <span>React.js</span></div>
+                                    <div className="skills_span_container"><img src={sassIcon}/> <span>Sass. Scss</span></div>
+                                    <div className="skills_span_container"><img src={gitIcon}/> <span>Git. GitHub</span></div>
+                                </div>
+                                <div className="skills_title">Дополнительно:</div>
+                                <div className="skills_subtitle">
+                                    - Опыт работы с Axios, Redux, REST API;<br/>
+                                    - Опыт работы с библиотеками React (react-router-dom, react-hook-form, react-framer-motion и др)<br/>
+                                    - Знание основных принципов проектирования пользовательских интерфейсов;<br/>
+                                    - Стремление к самообразованию и развитию профессиональных навыков;<br/>
+                                </div>
+                            </div>
+
+                            <div className={education ? "main_task_education_active" : "main_task_education"}>
+                                <div className="education_container">
+                                    <div className="skills_title">SkillFactory</div>
+                                    <div className="skillseducation_subtitle">
+                                        <span>Специализация:</span> Frontend разработчик<br/>
+                                        <span>Период обучения:</span> 26.09.2022 - 14.06.2023<br/>
+                                        <span>Изучил:</span> HTML5, CSS3, JavaScript, Git, GitHub, npm, Gulp, Sass, Scss, React.js<br/>
+                                        <span>Сертификат:</span> <Link target="blank" to="https://disk.yandex.ru/i/1VTKxMfmIF6sJA"> Открыть</Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
@@ -66,17 +126,6 @@ const About = () => {
 
             </div>
 
-            <div className="about_down_section">
-                <h2 className="about_down_section_title">
-                    Примеры всех моих работ вы можете посмотреть в разделе «Проекты». 
-                    Если вы не нашли там интересующего примера, я всегда готов ответить на ваши вопросы и предоставить дополнительную информацию о своих навыках и умениях, подтвержденных реальными проектами.
-                </h2>
-            </div>
-
-            <div className="about_footer">
-                <h3 className="about_footer_text">Сертификат, подтверждающий квалификацию</h3>
-                <Link target="_blank" to="https://disk.yandex.ru/i/1VTKxMfmIF6sJA"><img src={cvIcom} alt="cv" className="about_footer_icon"/></Link>
-            </div>
         </section>
     )
 }
