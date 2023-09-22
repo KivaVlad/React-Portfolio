534636bbefbcfa8113e7e43252643a8e80f3a5b7
KivaVlad <melnikovv717@gmail.com>
2023-09-22 23:47:04 +0300

    Обновил ссылку для резюме

--- src/pages/ContactsPage/Contacts.jsx
+++ src/pages/ContactsPage/Contacts.jsx
@@ -59,7 +59,7 @@ const Contacts = () => {
                                 </div>
                             </Link>
                         </div>
-                        <Link target='_blank' to="https://disk.yandex.ru/i/orhuVKc0uZmaoA" className='contacts_links'>
+                        <Link target='_blank' to="https://disk.yandex.ru/i/1gF6RplNWFjGpQ" className='contacts_links'>
                             <div className='contacts_cv'>
                                 <img src={cvIcon} alt='cv' />
                                 <h3 className="contacts_cv_title">Резюме</h3>
